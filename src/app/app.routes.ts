8b73bb8e3f9305d90e562bf3849bb776cc63749d
Antivery <antivery@gmail.com>
2025-03-02 21:00:37 -0500

    edit dashboard a little

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { LeaseManagementComponent } from './components/lease-managment/lease-managment-component/lease-management.component';
 import { LeaseDetailsComponent } from './components/lease-managment/lease-details-component/lease-details.component';
+import { DashboardComponent } from './components/dashboard-component/dashboard.component';
 
 export const routes: Routes = [
     { path: 'lease-management', component: LeaseManagementComponent },
-    { path: 'lease-details', component: LeaseDetailsComponent }
+    { path: 'lease-details', component: LeaseDetailsComponent },
+    { path: 'dashboard', component: DashboardComponent}
 ];
