8ffc89cf390b1aabe93627141d4746870bb5bdb7
Antivery <antivery@gmail.com>
2025-03-02 20:51:35 -0500

    added update lease func

--- src/app/components/lease-managment/lease-details-component/lease-details.component.ts
+++ src/app/components/lease-managment/lease-details-component/lease-details.component.ts
@@ -1,5 +1,6 @@
 import { Component, Inject } from '@angular/core';
-import { MAT_DIALOG_DATA, MatDialogRef, MatDialogModule } from '@angular/material/dialog';
+import { MAT_DIALOG_DATA, MatDialogRef, MatDialogModule, MatDialog } from '@angular/material/dialog';
+import { LeaseFormComponent } from '../lease-form-component/lease-form.component';
 
 @Component({
   selector: 'app-lease-details-component',
@@ -8,18 +9,31 @@ import { MAT_DIALOG_DATA, MatDialogRef, MatDialogModule } from '@angular/materia
   styleUrl: './lease-details.component.css'
 })
 export class LeaseDetailsComponent {
-  constructor( public dialogRef: MatDialogRef<LeaseDetailsComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: any) {}
+  constructor(
+    public dialogRef: MatDialogRef<LeaseDetailsComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: any,
+    private dialog: MatDialog
+  ) {}
 
     closeDialog() {
       this.dialogRef.close();
     }
     
     updateLeaseDetails() {
+      this.dialog.open(LeaseFormComponent, {
+        height: '90%',
+        data: this.data
+      });
 
+    this.dialogRef.afterClosed().subscribe(updatedLease => {
+        if (updatedLease) {
+          console.log('Lease Updated:', updatedLease);
+          this.dialogRef.close(updatedLease); // Close details modal and pass back updated data
+        }
+      });
     }
-  
+
     deleteLease() {
-      
+
     }
 }
